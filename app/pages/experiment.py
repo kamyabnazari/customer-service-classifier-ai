6f867f84f910525e942229712cef69d349b78d32
Kamyab Nazari <kamyabnazari@icloud.com>
2024-07-10 19:15:40 +0200

    Minor changes

--- app/pages/experiment.py
+++ app/pages/experiment.py
@@ -2,7 +2,7 @@ import streamlit as st
 from services.openai_service import generate_response
 from state import global_state
 
-st.title("Experiment Page")
+st.title("Experiment")
 
 # Check if a dataset is loaded
 if not st.session_state.get("dataset_loaded", False):
@@ -16,15 +16,20 @@ else:
         # Input field for user to enter their prompt
         user_input = st.text_input("Enter your prompt:")
 
+    prompt_output = ""
+
     with col2:
         # Button to submit the prompt
         if st.button("Generate", use_container_width=True):
             if user_input:
                 # Generate the response from OpenAI
-                output = generate_response(user_input)
-                # Display the response
-                st.write(f"Response: {output}")
+                prompt_output = generate_response(user_input)
+
             else:
                 st.write("Please enter a prompt.")
-    
+
     st.divider()
+
+    # Display the response
+    if prompt_output != "":
+        st.write(f"Response: {prompt_output}")
