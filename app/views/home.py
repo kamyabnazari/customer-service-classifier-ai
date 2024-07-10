2c0b7284c1ca938ba878ff3d3908609716743147
Kamyab Nazari <kamyabnazari@icloud.com>
2024-07-10 17:57:23 +0200

    Update home layout

--- app/views/home.py
+++ app/views/home.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 from services.data_service import list_datasets, load_datasets
 from state import global_state
@@ -14,61 +13,51 @@ datasets = list_datasets(data_dir)
 # Capitalize the first letter of each dataset name
 datasets_capitalized = [dataset.capitalize() for dataset in datasets]
 
-# Select dataset
-selected_dataset = st.selectbox("Select a dataset", datasets_capitalized)
+col1, col2 = st.columns([4, 1], vertical_alignment="bottom")
 
-# Initialize session state for load button disabled state
-if "load_disabled" not in st.session_state:
-    st.session_state.load_disabled = False
-
-# Layout for buttons
-col1, col2 = st.columns([1, 4])
-
-# Load dataset button
 with col1:
-    if st.button("Load Dataset", disabled=st.session_state.load_disabled):
-        try:
-            if selected_dataset:
-                # Convert back to lowercase to match the folder names
-                selected_dataset = selected_dataset.lower()
-                global_state.datasets = load_datasets(selected_dataset, data_dir)
-                st.session_state.load_disabled = True
-                st.success("Dataset loaded successfully!")
-                st.rerun()
-        except Exception as e:
-            st.error(f"Failed to load dataset: {e}")
+    # Select dataset
+    selected_dataset = st.selectbox("Select a dataset", datasets_capitalized)
 
-# Delete dataset button
+# Initialize session state for dataset loaded state
+if "dataset_loaded" not in st.session_state:
+    st.session_state.dataset_loaded = False
+
 with col2:
-    if st.button("Delete Dataset", type="primary", disabled=not st.session_state.load_disabled):
-        global_state.datasets.clear()
-        st.session_state.load_disabled = False
-        st.success("Dataset deleted successfully!")
-        st.rerun()
+    # Conditionally render buttons
+    if not st.session_state.dataset_loaded:
+        # Load dataset button
+        if st.button("Load Dataset"):
+            try:
+                if selected_dataset:
+                    # Convert back to lowercase to match the folder names
+                    selected_dataset = selected_dataset.lower()
+                    global_state.datasets = load_datasets(selected_dataset, data_dir)
+                    st.session_state.dataset_loaded = True
+                    st.success("Dataset loaded successfully!")
+                    st.rerun()
+            except Exception as e:
+                st.error(f"Failed to load dataset: {e}")
+    else:
+        # Delete dataset button
+        if st.button("Delete Dataset", type="primary"):
+            global_state.datasets.clear()
+            st.session_state.dataset_loaded = False
+            st.success("Dataset deleted successfully!")
+            st.rerun()
+
+col1, col2, col3 = st.columns([1, 1, 1])
 
 # Display a sample of the loaded data
 if global_state.datasets:
-    data_stats = []
-    if "test" in global_state.datasets:
-        data_stats.append({
-            "File": "test.csv",
-            "Total Elements": len(global_state.datasets["test"])
-        })
+    with col1:
+        if "test" in global_state.datasets:
+            st.metric(label="Total Test Elements", value=len(global_state.datasets["test"]))
 
-    if "fine_tuning" in global_state.datasets:
-        data_stats.append({
-            "File": "fine_tuning.csv",
-            "Total Elements": len(global_state.datasets["fine_tuning"])
-        })
+    with col2:
+        if "fine_tuning" in global_state.datasets:
+            st.metric(label="Total Fine Tuning Elements", value=len(global_state.datasets["fine_tuning"]))
 
-    if "categories" in global_state.datasets:
-        data_stats.append({
-            "File": "categories.json",
-            "Total Elements": len(global_state.datasets["categories"])
-        })
-
-    # Display data statistics
-    if data_stats:
-        st.write("Data Statistics")
-        stats_df = pd.DataFrame(data_stats)
-        st.dataframe(stats_df)
+    with col3:
+        if "categories" in global_state.datasets:
+            st.metric(label="Total Category Elements", value=len(global_state.datasets["categories"]))
